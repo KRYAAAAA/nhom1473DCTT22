90ed1e35109dc72d43006f6395de11521762e97f
KRYAAAA <khoak111n@gmail.com>
2025-04-14 17:30:36 +0700

    them file css vs hinh anh

---
+++ menutrai.php
@@ -0,0 +1,10 @@
+<div class="menutrai">
+    <ul>
+        <li style="font-weight: bold;font-size: 130%; height: 60px; background-color: rgb(26, 39, 87);">DANH MỤC</li>
+        <li><a href="http://localhost/QuanLyTrangThietBi/Tam/FinalExam/Home.php">Cơ sở vật chất</a></li>
+        <li><a href="http://localhost/QuanLyTrangThietBi/Duc/muontra_admin/muontra_admin.php">Mượn trả</a></li>
+        <li><a href="http://localhost/QuanLyTrangThietBi/Duc/hethong/hethong.php">Hệ thống</a></li>
+        <li><a href="http://localhost/QuanLyTrangThietBi/Duc/thietbiphongban/phongban.php">Thiết bị phòng ban</a></li>
+        <li><a href="http://localhost/QuanLyTrangThietBi/Duc/thongke/thongke.php">Thống kê</a></li>
+    </ul>
+</div>
